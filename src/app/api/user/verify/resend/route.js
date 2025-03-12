d7cc1710b80f9e1cb6dbd1a4bdee9cb45453770d
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-03-12 10:05:24 +0600

    resend email

--- src/app/api/user/verify/resend/route.js
+++ src/app/api/user/verify/resend/route.js
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server"
 import jwt from 'jsonwebtoken'
+import { sendEmail } from "@/utils/sendMail";
 
 export const GET = async () => {
+    const cookie = await request.cookies;
+    const userMail = cookie.get('email')?.value;
+    const userMobile = cookie.get('mobile')?.value;
     try {
         const response = NextResponse.json({ message: 'Otp resend successful', success: true });
 
         const otp = Math.floor(100000 + Math.random() * 900000).toString();
-        console.log(otp);
         const hashedOtp = jwt.sign({ otp }, process.env.JWT_SECRET, { expiresIn: '2m' });
+
+        if (userMail) {
+            await sendEmail({ email:userMail, otp });
+        }
 
         response.cookies.set('otp', hashedOtp, {
             httpOnly: true,
@@ -18,7 +25,7 @@ export const GET = async () => {
         });
 
         return response;
-        
+
     } catch (error) {
         return NextResponse.json({ message: 'Failed to resend otp', success: false });
     }
