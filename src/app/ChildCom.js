939bc36053179bddc5c561c2b87a00d397119690
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-03-17 19:20:24 +0600

    add last code

--- src/app/ChildCom.js
+++ src/app/ChildCom.js
@@ -10,20 +10,11 @@ const ChildCom = ({ children }) => {
     const hiddenPath = ['/office'];
     const setHiddenPath = hiddenPath.includes(pathName);
 
-    useEffect(() => {
-        document.title = 'your server is down'
-    },[]);
-
     return (
-        // <div className='w-full h-screen scroll-smooth'>
-        //     {(!routePath && !setHiddenPath) && <Header />}
-        //     <div className='mt-16'>{children}</div>
-        //     {(!routePath && !setHiddenPath) &&  <Footer />}
-        // </div>
-        <div className='w-full h-screen flex flex-col gap-y-4 items-center justify-center bg-[#000211]'>
-            {/* <Home/> */}
-            <img src='/404-error-page-animation-download-in-lottie-json-gif-static-svg-file-formats--loading-not-found-the-ultimate-pack-design-development-animations-3299952.webp' alt='not found' />
-            <h1 className='text-4xl font-semibold text-blue-800'>Your server is down</h1>
+        <div className='w-full h-screen scroll-smooth'>
+            {(!routePath && !setHiddenPath) && <Header />}
+            <div className='mt-16'>{children}</div>
+            {(!routePath && !setHiddenPath) &&  <Footer />}
         </div>
     )
 }
