aa5c5a4d1c27fc160b42f8b4d5d8aee2d5cf340e
Nahid Hasan <146347290+alimnh520@users.noreply.github.com>
2025-09-07 00:14:08 +0600

    Update route.js

--- src/app/api/user/del-data/delete-uddokta/route.js
+++ src/app/api/user/del-data/delete-uddokta/route.js
@@ -1,8 +1,10 @@
-
+import { ObjectId } from "mongodb";
+import { dbConnection } from "../../../../../../lib/connectDB";
 export const POST = async (request) => {
     try {
         const { uddoktaId } = await request.json();
-        
+        const collection = (await dbConnection()).collection('admin');
+        await collection.deleteOne({ _id: new ObjectId(uddoktaId) });
         return NextResponse.json({ message: 'successful', success: true });
     } catch (error) {
         console.log(error);
