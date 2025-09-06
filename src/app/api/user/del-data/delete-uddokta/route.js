093407e7d1df0e041f742674222b31a97bcdb7e4
Nahid Hasan <146347290+alimnh520@users.noreply.github.com>
2025-09-07 00:04:01 +0600

    Update route.js

--- src/app/api/user/del-data/delete-uddokta/route.js
+++ src/app/api/user/del-data/delete-uddokta/route.js
@@ -1,1 +1,11 @@
 
+export const POST = async (request) => {
+    try {
+        const { uddoktaId } = await request.json();
+        
+        return NextResponse.json({ message: 'successful', success: true });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: 'failed', success: false });
+    }
+}
